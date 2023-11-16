7a3520e2fc899eaf6a0e8f3e32506fcd4ef59712
Omar <saucyfunnel@gmail.com>
2023-11-15 19:34:17 -0500

    semana 13

--- semana13/src/app/app.component.spec.ts
+++ semana13/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'appdemo' title`, () => {
+  it(`should have the 'semana13' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('appdemo');
+    expect(app.title).toEqual('semana13');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, appdemo');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, semana13');
   });
 });
